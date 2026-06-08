34eb3ea415c868b89b136ce30acea55391e94368
Gareth Jones <gjones@quora.com>
2026-06-08 18:06:05 +0000

    Verify TypeScript package usage

---
+++ test/types/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import BookingSelector, {
+  BookingSelector as NamedBookingSelector,
+  type BookingSelectorProps
+} from '../../src/lib'
+
+const selection = [new Date('2026-06-08T09:00:00.000Z')]
+const blocked = [new Date('2026-06-08T10:00:00.000Z').getTime()]
+
+const renderDateCell: BookingSelectorProps['renderDateCell'] = (time, selected, blockedCell) => (
+  <span data-time={time.toISOString()} data-selected={selected} data-blocked={blockedCell} />
+)
+
+const onChange: BookingSelectorProps['onChange'] = nextSelection => {
+  nextSelection.forEach(time => {
+    time.toISOString()
+  })
+}
+
+const defaultExport = (
+  <BookingSelector
+    selection={selection}
+    blocked={blocked}
+    selectionScheme="square"
+    onChange={onChange}
+    startDate={new Date('2026-06-08T00:00:00.000Z')}
+    numDays={3}
+    minTime={9}
+    maxTime={17}
+    dateFormat="MMM d"
+    margin={4}
+    unselectedColor="#dbedff"
+    selectedColor="rgba(89, 154, 242, 1)"
+    hoveredColor="rgba(162, 198, 248, 1)"
+    blockedColor="rgba(79, 79, 79, 1)"
+    renderDateCell={renderDateCell}
+  />
+)
+
+const namedExport = <NamedBookingSelector selection={selection} onChange={onChange} />
+
+void defaultExport
+void namedExport
